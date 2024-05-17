b393da8ce17004a01fe6baba2a3d8071fa2bb92e
Jimi Chhatrala <jimichhatrala8488@gmail.com>
2024-05-17 18:46:13 +0530

    Post Category Added

--- src/Components/Admin/AdminCategory/index.jsx
+++ src/Components/Admin/AdminCategory/index.jsx
@@ -7,6 +7,49 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function AdminCategory(){
+    // Add New Category
+    //-----------------------------------------------------------------------
+
+    const [category_name , setCategoryName] = useState();
+    const [URL , setURL] = useState();
+
+    const fd = new FormData()
+
+    const addCategory = async(e) => {
+        e.preventDefault();
+        
+        fd.append('URL', URL);
+        fd.append('fileName', URL.name);
+        fd.append('category_name', category_name);
+
+        await axios.post('http://localhost:3046/api/v1/admin/addcategory',
+        fd,
+         {
+            headers:{
+                Authorization: localStorage.getItem('accessToken'),
+                'content-type': 'multipart/form-data',
+            },
+        }
+    )
+        .then((response) => {
+            console.log('Response: ', response)
+    
+            if(response.data.success) {
+                alert(response.data.message)
+            } else {
+                alert(response.data.message)
+            }
+        }).catch((error) => {
+            console.log(error);
+            alert(error.response.data.message);
+        })
+    }
+    //-----------------------------------------------------------------------
+
+
+    // Get All Category Start
+    //-----------------------------------------------------------------
+
     const [allCategory, setAllCategory] = useState([]); 
 
     useEffect(() => {    
@@ -16,6 +59,9 @@ export default function AdminCategory(){
             // console.log(response.data.message)
         }).catch((error)=>console.log(error))
     }, []);
+
+    // Get All Category End
+    //-----------------------------------------------------------------
 
     return(
         <div>
@@ -28,11 +74,11 @@ export default function AdminCategory(){
                 <div className={styles.form_container}>
                     <form action="#">
                         <p className={styles.file_input}>
-                            <input type="file" name="file" id="file" />
+                            <input onChange={(e)=>setURL(e.target.files[0])} type="file" name="file" id="file" />
                             <label>CHOOSE PIC</label>
                         </p>
-                        <input type="text" name='title' id='title' placeholder='Category name *' />
-                        <input type="submit" value="POST" />
+                        <input type="text" name='title' id='title' placeholder='Category name *' value={category_name} onChange={(e)=> setCategoryName(e.target.value)} />
+                        <input type="submit" value="POST" onClick={addCategory} />
                     </form>
                 </div>
             <div className={styles.fixTableHead}>
