717a556b889c8693bdb00f6a91f036b77b7b93ac
Jimi Chhatrala <jimichhatrala8488@gmail.com>
2024-05-22 19:29:26 +0530

    Events Page Completed

--- src/Components/Events/index.jsx
+++ src/Components/Events/index.jsx
@@ -2,8 +2,29 @@ import styles from './style.module.css';
 import Footer from '../../Components/Footer';
 import NavBar from '../NavBar';
 import { useNavigate } from 'react-router-dom';
+import Image1 from '../../images/events/event-1.jpg';
+import Image2 from '../../images/events/event-2.jpg';
+import Image3 from '../../images/events/event-3.jpg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Events(){
+
+    //-----------------------------------------------------------------
+    // Get All Category Start
+
+    const [allCategory, setAllCategory] = useState([]); 
+
+    useEffect(() => {    
+        axios.get('http://localhost:3046/api/v1/admin/showcategory')
+        .then((response) => {
+            setAllCategory(response.data.message)
+        }).catch((error)=>console.log(error))
+    }, []);
+
+    // Get All Category End
+    //-----------------------------------------------------------------
+
   const navigate = useNavigate();
     return(
         <div>
@@ -11,12 +32,32 @@ export default function Events(){
             <section id={styles.hero}></section>
             <section className={styles.events}>
                 <div className={styles.events_container}>
-                    <div className={`${styles.events_image} ${styles.events_image_1}`}>
+                    {
+                        allCategory ? allCategory.map((element, index)=>{
+                            return(
+                                <div className={`${styles.events_image}`}>
+                                    <img src={element.URL} alt='Category Image' />
+                                    <div className={styles.events_text}>
+                                        <p className={styles.events_text_1} onClick={()=>navigate('/events/event')}>{element.category_name}</p>
+                                    </div>
+                                </div>
+                            )
+                        }) : null
+                    }
+                    
+                    {/* <div className={`${styles.events_image}`}>
+                        <img src={Image2} alt='Category Image' />
                         <div className={styles.events_text}>
                             <p className={styles.events_text_1} onClick={()=>navigate('/events/event')}>Concert</p>
                         </div>
                     </div>
-                    <div className={`${styles.events_image} ${styles.events_image_2}`}>
+                    <div className={`${styles.events_image}`}>
+                        <img src={Image3} alt='Category Image' />
+                        <div className={styles.events_text}>
+                            <p className={styles.events_text_1} onClick={()=>navigate('/events/event')}>Concert</p>
+                        </div>
+                    </div> */}
+                    {/* <div className={`${styles.events_image} ${styles.events_image_2}`}>
                         <div className={styles.events_text}>
                             <p className={styles.events_text_1} onClick={()=>navigate('/events/event')}>Orchestra</p>
                         </div>
@@ -40,7 +81,7 @@ export default function Events(){
                         <div className={styles.events_text}>
                             <p className={styles.events_text_1} onClick={()=>navigate('/events/event')}>Work</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </section>
             <Footer />
