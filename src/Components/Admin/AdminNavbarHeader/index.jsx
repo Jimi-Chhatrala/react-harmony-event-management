1f33ddb716850e6fb6f198aace349562e2a0cc7b
Jimi-Chhatrala <jimichhatrala8488@gmail.com>
2024-05-30 17:37:58 +0530

    Admin Profile Update Name/Email Completed

--- src/Components/Admin/AdminNavbarHeader/index.jsx
+++ src/Components/Admin/AdminNavbarHeader/index.jsx
@@ -1,109 +1,138 @@
-import styles from './style.module.css';
+import styles from "./style.module.css";
 import { PiUserListFill } from "react-icons/pi";
 import { IoMdCalendar } from "react-icons/io";
 import { CiBookmarkPlus } from "react-icons/ci";
 import { FaShapes } from "react-icons/fa6";
 import { LuLogOut } from "react-icons/lu";
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import Cookies from "js-cookie";
+import axios from "axios";
+import { RiAdminFill } from "react-icons/ri";
 
-const adminToken = Cookies.get('adminAccessToken');
+const adminToken = Cookies.get("adminAccessToken");
 
-export default function AdminNavbarHeader(){
-    const [currentAdmin, setCurrentAdmin] = useState();
+export default function AdminNavbarHeader() {
+  const [currentAdmin, setCurrentAdmin] = useState();
 
-    const navigate = useNavigate();
-    const adminLogout = (e) => {
-        e.preventDefault();
-    
-        Cookies.remove('adminAccessToken')
-        navigate('/admin/login')
-      }
+  const navigate = useNavigate();
+  const adminLogout = (e) => {
+    e.preventDefault();
 
-    // -------------------------------------------------------
-    //   Get Current Admin
+    Cookies.remove("adminAccessToken");
+    navigate("/admin/login");
+  };
 
-    const getCurrentAdmin = async () => {
-        await axios.get('http://localhost:3046/api/v1/admin/getcurrentAdmin', {
-            headers:{
-                Authorization: adminToken,
-            },
-        })
-        .then((response) => {
-            setCurrentAdmin(response.data.data)
-            console.log('Current Admin Data',response.data.data)
-        }).catch((error)=>console.log(error))
-    }
+  // -------------------------------------------------------
+  //   Get Current Admin
 
-    useEffect(() => {    
-     getCurrentAdmin();   
-    }, []);
+  const getCurrentAdmin = async () => {
+    await axios
+      .get("http://localhost:3046/api/v1/admin/getcurrentAdmin", {
+        headers: {
+          Authorization: adminToken,
+        },
+      })
+      .then((response) => {
+        setCurrentAdmin(response.data.data);
+        console.log("Current Admin Data", response.data.data);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    //   Get Current Admin
-    // -------------------------------------------------------
+  useEffect(() => {
+    getCurrentAdmin();
+  }, []);
 
+  //   Get Current Admin
+  // -------------------------------------------------------
 
   const navRef = useRef();
   const showNavbar = () => {
     navRef.current.classList.toggle(styles.responsive_nav);
     console.log("shownavbar");
   };
-  
-    return(
-        <div>
-            <section className={styles.admin_container}>
-                <div className={`${styles.navbar}`}  ref={navRef}>
-                <button className={`${styles.navbar_btn} ${styles.nav_close_btn}`} onClick={showNavbar}>
-                            <FaTimes />
-                        </button>
-                    <hr className={styles.first_hr} />
-                    <div>
-                        <ul>
 
-                            <li className={styles.nav_btn}>
-                                <Link className={styles.a} to="/admin/all-user"><PiUserListFill className={styles.nav_btn_icon} /><span className={styles.nav_btn_text}>User List</span></Link>
-                            </li>
-                            <li className={styles.nav_btn}>
-                                <Link className={styles.a} to="/admin/add-event"><IoMdCalendar className={styles.nav_btn_icon} /><span className={styles.nav_btn_text}>Event Post</span></Link>
-                            </li>
-                            <li className={styles.nav_btn}>
-                                <Link className={styles.a} to="/my-bookings"><CiBookmarkPlus className={styles.nav_btn_icon} /><span className={styles.nav_btn_text}>Book Events</span></Link>
-                            </li>
-                            <li className={styles.nav_btn}>
-                                <Link className={styles.a} to="/admin/category"><FaShapes className={styles.nav_btn_icon} /><span className={styles.nav_btn_text}>Post Category</span></Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <hr />
-                    <p>Authentication</p>
-                    <ul>
-                        <li className={styles.nav_btn}>
-                            <Link className={styles.a} onClick={adminLogout}><LuLogOut className={styles.nav_btn_icon} /><span className={styles.nav_btn_text}>Log Out</span></Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className={styles.header}>
-                    <div>
-                        <button className={styles.navbar_btn} onClick={showNavbar}>
-                            <FaBars />
-                        </button>
-                        <h3 className={styles.admin_text}>Admin</h3>
-                    </div>
-                    <div>
-                        <div className={styles.admin_details_container}>
-                            <img src="https://res.cloudinary.com/dtdlad1ud/image/upload/v1703939018/yl9frkeayfp9wftlfz8l.jpg" alt="" className={styles.admin_image} />
-                            <div>
-                                <p className={styles.admin_text}>{currentAdmin?.fullName}</p>
-                                <p className={styles.admin_text}>{currentAdmin?.email}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <Outlet />
+  return (
+    <div>
+      <section className={styles.admin_container}>
+        <div className={`${styles.navbar}`} ref={navRef}>
+          <button
+            className={`${styles.navbar_btn} ${styles.nav_close_btn}`}
+            onClick={showNavbar}
+          >
+            <FaTimes />
+          </button>
+          <hr className={styles.first_hr} />
+          <div>
+            <ul>
+              <li className={styles.nav_btn}>
+                <Link className={styles.a} to="/admin/all-user">
+                  <PiUserListFill className={styles.nav_btn_icon} />
+                  <span className={styles.nav_btn_text}>User List</span>
+                </Link>
+              </li>
+              <li className={styles.nav_btn}>
+                <Link className={styles.a} to="/admin/add-event">
+                  <IoMdCalendar className={styles.nav_btn_icon} />
+                  <span className={styles.nav_btn_text}>Event Post</span>
+                </Link>
+              </li>
+              <li className={styles.nav_btn}>
+                <Link className={styles.a} to="/my-bookings">
+                  <CiBookmarkPlus className={styles.nav_btn_icon} />
+                  <span className={styles.nav_btn_text}>Book Events</span>
+                </Link>
+              </li>
+              <li className={styles.nav_btn}>
+                <Link className={styles.a} to="/admin/category">
+                  <FaShapes className={styles.nav_btn_icon} />
+                  <span className={styles.nav_btn_text}>Post Category</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <hr />
+          <p>Authentication</p>
+          <ul>
+            <li className={styles.nav_btn}>
+              <Link className={styles.a} to="/admin/profile">
+                <RiAdminFill className={styles.nav_btn_icon} />
+                <span className={styles.nav_btn_text}>Admin Profile</span>
+              </Link>
+            </li>
+            <li className={styles.nav_btn}>
+              <Link className={styles.a} onClick={adminLogout}>
+                <LuLogOut className={styles.nav_btn_icon} />
+                <span className={styles.nav_btn_text}>Log Out</span>
+              </Link>
+            </li>
+          </ul>
         </div>
-    );
+        <div className={styles.header}>
+          <div>
+            <button className={styles.navbar_btn} onClick={showNavbar}>
+              <FaBars />
+            </button>
+            <h3 className={styles.admin_text}>Admin</h3>
+          </div>
+          <div>
+            <div className={styles.admin_details_container}>
+              <img
+                src="https://res.cloudinary.com/dtdlad1ud/image/upload/v1703939018/yl9frkeayfp9wftlfz8l.jpg"
+                alt=""
+                className={styles.admin_image}
+              />
+              <div>
+                <p className={styles.admin_text}>{currentAdmin?.fullName}</p>
+                <p className={styles.admin_text}>{currentAdmin?.email}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Outlet />
+    </div>
+  );
 }
--- src/Components/Admin/AdminProfile/index.jsx
+++ src/Components/Admin/AdminProfile/index.jsx
@@ -1,75 +1,141 @@
-import styles from './style.module.css';
-import AdminNavbarHeader from '../AdminNavbarHeader';
-import { useState } from 'react';
+import styles from "./style.module.css";
+import AdminNavbarHeader from "../AdminNavbarHeader";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 
-function PersonalDetailPage(){
-    return(
-        <div className={styles.admin_profile_details_container}>
-            <h3>Personal Details</h3>
-            <form action="#">
-                <div>
+function PersonalDetailPage() {
+  const [currentAdmin, setCurrentAdmin] = useState();
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
 
-                <label htmlFor="fullname">Full Name:</label>
-                <input type="text" id='fullname' name='fullname' />
-                </div>
-                <div>
+  useEffect(() => {
+    setFullName(currentAdmin?.fullName);
+    setEmail(currentAdmin?.email);
+  }, [currentAdmin]);
 
-                <label htmlFor="email">Email Address:</label>
-                <input type="email" id='email' name='email' />
-                </div>
-                <input type="button" value="Update" />
-            </form>
-        </div>
-    );
-}
+  const getCurrentAdmin = async () => {
+    axios
+      .get("http://localhost:3046/api/v1/admin/getcurrentAdmin", {
+        headers: {
+          Authorization: Cookies.get("adminAccessToken"),
+        },
+      })
+      .then((response) => {
+        setCurrentAdmin(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch((error) => console.log(error));
+  };
 
-function ChangePasswordPage(){
-    return(
-        <div className={styles.admin_profile_details_container}>
-            <h3>Change Password</h3>
-            <form action="#">
-                <div>
+  useEffect(() => {
+    getCurrentAdmin();
+  }, []);
 
-                <label htmlFor="old-password">Old Password:</label>
-                <input type="password" id='old-password' name='old-password' />
-                </div>
-                <div>
+  const updateAdminDetails = async (e) => {
+    e.preventDefault();
+    const data = { fullName, email };
 
-                <label htmlFor="new-password">New Password:</label>
-                <input type="password" id='new-password' name='new-password' />
-                </div>
-                <div>
+    await axios
+      .post("http://localhost:3046/api/v1/admin/update", data, {
+        headers: {
+          Authorization: Cookies.get("adminAccessToken"),
+        },
+      })
+      .then((response) => {
+        if (response.data.success) {
+          alert(response.data.message);
+        } else {
+          alert(response.data.message);
+        }
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  };
 
-                <label htmlFor="confirm-new-password">Confirm New Password:</label>
-                <input type="password" id='confirm-new-password' name='confirm-new-password' />
-                </div>
-                <input type="button" value="Update" />
-            </form>
-        </div>
-    );
-}
-
-export default function AdminProfile(){
-    const [activeButton, setActiveButton] = useState('personalDetail');
-    const handleClick = (button) => {
-        setActiveButton(button);
-    };
-
-    return(
+  return (
+    <div className={styles.admin_profile_details_container}>
+      <h3>Personal Details</h3>
+      <form action="#">
         <div>
-            <AdminNavbarHeader/>
-            <section className={styles.admin_profile_container}>
-                <div className={styles.admin_profile_image_container}>
-                    <img src="" alt="" />
-                </div>
-                <div className={styles.action_buttons}>
-                    <button onClick={() => handleClick('personalDetail')}>Personal Detail</button>
-                    <button onClick={() => handleClick('changePassword')}>Change Password</button>
-                </div>
-                {activeButton === 'personalDetail' && <PersonalDetailPage />}
-                {activeButton === 'changePassword' && <ChangePasswordPage />}
-
-            </section>
+          <label htmlFor="fullname">Full Name:</label>
+          <input
+            type="text"
+            id="fullname"
+            name="fullname"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
         </div>
-    );
+        <div>
+          <label htmlFor="email">Email Address:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <input type="button" value="Update" onClick={updateAdminDetails} />
+      </form>
+    </div>
+  );
+}
+
+function ChangePasswordPage() {
+  return (
+    <div className={styles.admin_profile_details_container}>
+      <h3>Change Password</h3>
+      <form action="#">
+        <div>
+          <label htmlFor="old-password">Old Password:</label>
+          <input type="password" id="old-password" name="old-password" />
+        </div>
+        <div>
+          <label htmlFor="new-password">New Password:</label>
+          <input type="password" id="new-password" name="new-password" />
+        </div>
+        <div>
+          <label htmlFor="confirm-new-password">Confirm New Password:</label>
+          <input
+            type="password"
+            id="confirm-new-password"
+            name="confirm-new-password"
+          />
+        </div>
+        <input type="button" value="Update" />
+      </form>
+    </div>
+  );
+}
+
+export default function AdminProfile() {
+  const [activeButton, setActiveButton] = useState("personalDetail");
+
+  const handleClick = (button) => {
+    setActiveButton(button);
+  };
+
+  return (
+    <div>
+      <AdminNavbarHeader />
+      <section className={styles.admin_profile_container}>
+        <div className={styles.admin_profile_image_container}>
+          <img src="" alt="" />
+        </div>
+        <div className={styles.action_buttons}>
+          <button onClick={() => handleClick("personalDetail")}>
+            Personal Detail
+          </button>
+          <button onClick={() => handleClick("changePassword")}>
+            Change Password
+          </button>
+        </div>
+        {activeButton === "personalDetail" && <PersonalDetailPage />}
+        {activeButton === "changePassword" && <ChangePasswordPage />}
+      </section>
+    </div>
+  );
 }
